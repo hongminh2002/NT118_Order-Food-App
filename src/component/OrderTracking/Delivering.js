9c70ae39cab807e69deb4a17c9cbf4cbe76c6c43
Phan Thanh Bách <phanbach02@gmail.com>
2023-05-01 02:14:28 +0700

    Thêm component
    Thêm component cho màn hình Theo dõi đơn hàng

---
+++ src/component/OrderTracking/Delivering.js
@@ -0,0 +1,86 @@
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { useFonts } from "expo-font";
+import AppLoading from 'expo-app-loading';
+
+const orders = [
+    {
+        image: require("../../asset/FoodImage/cheese-burger-deluxe.png"),
+        foodname: 'Burger bò phô-mai đặc biệt x2',
+        total: '110,000',
+        time: '15:45',
+        date: '31/3/2023',
+    },
+    {
+        image: require("../../asset/FoodImage/mcspicy-deluxe.png"),
+        foodname: 'Burger phi lê gà cay đặc biệt',
+        total: '101,000',
+        time: '14:00',
+        date: '31/3/2023',
+    },
+]
+
+const Delivering = () => {
+
+    const [fontsLoaded] = useFonts({
+        "Roboto-Bold": require("../../asset/fonts/Roboto-Bold.ttf"),
+        "Roboto-Medium": require("../../asset/fonts/Roboto-Medium.ttf"),
+        "Roboto-Regular": require("../../asset/fonts/Roboto-Regular.ttf"),
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+    }
+
+    return (
+        <View >
+            {orders.map((order, index) => (
+                <View key={index} style={styles.infobox}>
+                    <View style={{ flexDirection: 'row', marginLeft: 15, marginRight: 10, alignItems: 'center', justifyContent: 'space-between', }}>
+                        <Image source={order.image} style={styles.img} />
+                        <View style={{ flexDirection: 'column', justifyContent: 'space-between', width: 200, }}>
+                            <Text style={{ fontFamily: "Roboto-Medium", fontSize: 14, }}>{order.foodname}</Text>
+                            <Text style={{ fontFamily: "Roboto-Regular", fontSize: 13, }}>{order.time} - {order.date}</Text>
+                        </View>
+                        <Text style={{ fontFamily: "Roboto-Regular", fontSize: 13, }}>{order.total} VNĐ</Text>
+                    </View>
+                    <View style={styles.horizontalline}></View>
+                </View>
+            ))}
+        </View>
+    )
+}
+
+const deviceWidth = Math.round(Dimensions.get("window").width);
+
+const styles = StyleSheet.create({
+    infobox: {
+        width: deviceWidth,
+        backgroundColor: 'white',
+        paddingTop: 10,
+    },
+
+    img: {
+        width: 75,
+        height: 60,
+
+    },
+
+    button: {
+        backgroundColor: '#rgba(255, 127, 63, 0.25)',
+        borderRadius: 12,
+        width: 100,
+        height: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    horizontalline: {
+        height: 1,
+        width: deviceWidth,
+        backgroundColor: '#D9D9D9',
+        marginTop: 15,
+    },
+});
+
+export default Delivering;
