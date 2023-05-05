3c2ee6fba18f5311241efbeabb2083c3520dc97f
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-05 13:21:37 +0700

    Update Cart.js

--- src/screen/Cart.js
+++ src/screen/Cart.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import Header from "../component/Header";
 import BottomTab from "../component/BottomTab";
+import CartItem from "../component/CartDetail/CartItem";
+import CheckOut from "../component/CartDetail/CheckOut";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Cart() {
   return (
     <SafeAreaView>
-      <Header />
-      <BottomTab />
+        <Header />
+        <CartItem />
+        <CheckOut />
+        <BottomTab />
     </SafeAreaView>
   );
 }
