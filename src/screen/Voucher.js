366f785719b3da7edc804bed5100f6b30884583c
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-05 13:22:07 +0700

    Update Voucher.js

--- src/screen/Voucher.js
+++ src/screen/Voucher.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import Header from "../component/Header";
 import BottomTab from "../component/BottomTab";
+import VoucherItem from "../component/VoucherDetail/VoucherItem";
+import ApplyVoucher from "../component/VoucherDetail/ApplyVoucher";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Voucher() {
   return (
     <SafeAreaView>
-      <View style={{ backgroundColor: "#EA5C2B", padding: 15 }}>
         <Header />
-      </View>
-      <BottomTab />
+        <VoucherItem />
+        <BottomTab />
     </SafeAreaView>
   );
 }
