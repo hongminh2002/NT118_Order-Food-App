c6b99c9cd88f919545db4f959143228a70f84a30
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-12 21:34:21 +0700

    Update CheckOut.js

--- src/component/CartDetail/CheckOut.js
+++ src/component/CartDetail/CheckOut.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Dimensions } from "react-native";
 import DatHangButton from "./DatHangButton";
+
+const windowWidth = Dimensions.get('window').width;
 
 export default function CheckOut(){
     return(
         <View style={{
             height:'12%',
+            width:windowWidth,
             borderRadius:12,
             backgroundColor:'#EA5C2B',
             flexDirection:'row',
-            alignContent:'center'
+            justifyContent:'space-between'
         }}>
-            <View style={{marginLeft:25, justifyContent:'center'}}>
+            <View style={{marginLeft:20, justifyContent:'center'}}>
                 <Text style={{color:'white', fontSize:17, fontWeight:"bold"}}>
                     Tổng thanh toán
                 </Text>
