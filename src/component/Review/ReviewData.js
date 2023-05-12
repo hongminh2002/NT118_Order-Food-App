bef287948462bb220972cba833e6b1422bb56664
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-12 23:33:35 +0700

    Create ReviewData.js

---
+++ src/component/Review/ReviewData.js
@@ -0,0 +1,26 @@
+export const reviews = [
+    {
+        id:1,
+        customerName:'Tâm Đan',
+        customerImage: require('../../asset/Woman1.png'),
+        starImage: require('../../asset/5Star.png'),
+        date:'23/04/2023',
+        description:'Thịt gà có lớp vỏ giòn, thịt mềm, mọng nước.'
+    },
+    {
+        id:2,
+        customerName:'Hoàng Nam',
+        customerImage: require('../../asset/Man1.png'),
+        starImage: require('../../asset/3Star.png'),
+        date:'05/03/2023',
+        description:'Lớp da hơi ỉu, thịt gà chưa được chín, còn khá dai.'
+    },
+    {
+        id:3,
+        customerName:'Ngọc Trâm',
+        customerImage: require('../../asset/Woman2.png'),
+        starImage: require('../../asset/4Star.png'),
+        date:'19/02/2023',
+        description:'Thịt khá mềm, lúc nhận được vẫn còn nóng. Quán đóng gói kĩ càng, cẩn thận.'
+    },
+];
