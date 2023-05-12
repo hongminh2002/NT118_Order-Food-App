7460a09a5cf0a0d4e949d1a6a0aac171f13884e6
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-12 20:11:32 +0700

    Update DatHangButton.js

--- src/component/CartDetail/DatHangButton.js
+++ src/component/CartDetail/DatHangButton.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { Text, TouchableOpacity } from "react-native";
 
 export default function DatHangButton(){
     return(
-        <TouchableOpacity style={{
-            padding:7,
+        <TouchableOpacity 
+        onPress={this.onPress}
+        style={{
             width:130,
+            height:'60%',
             marginTop:20,
             marginLeft:100,
             backgroundColor:'white',
-            borderRadius:12
+            borderRadius:12, 
+            justifyContent:'center', 
+            alignItems:'center'
             }}>
             <Text style={{
                 color:'#FF7F3F', 
                 fontWeight:'bold',
                 fontSize:16,
-                alignSelf:'center'}}>
+            }}>
                     Đặt hàng
             </Text>
         </TouchableOpacity>
