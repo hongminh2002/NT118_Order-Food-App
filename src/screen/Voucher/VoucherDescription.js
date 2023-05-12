09dfaf851428e2c1481e3e150f18521c0452ed98
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-12 23:40:05 +0700

    Add files via upload

---
+++ src/screen/Voucher/VoucherDescription.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeAreaView, ScrollView } from "react-native";
+import Header from "../../component/Header";
+import VoucherDes from "../../component/VoucherDetail/VoucherDes";
+import ApDungBigButton from "../../component/VoucherDetail/ApDungBigButton";
+
+export default function VoucherDescription(){
+    return(
+        <SafeAreaView style={{flex:1}}>
+            <Header />
+            <ScrollView>
+                <VoucherDes />
+            </ScrollView>
+            <ApDungBigButton />
+        </SafeAreaView>
+    );
+}
