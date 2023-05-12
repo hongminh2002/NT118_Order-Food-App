0f891fe8fdfac5dba6d6d1a49d5422616d0cd311
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-12 20:07:42 +0700

    Add file CartData

---
+++ src/component/CartDetail/CartData.js
@@ -0,0 +1,44 @@
+export const foods = [
+    {
+      id:1,
+      title : "Burger bò phô mai đặc biệt",
+      price:"49,000 VNĐ",
+      image : require('../../asset/BurgerBoDacBiet.png'),
+      quantity:"2"
+    },
+    {
+      id:2,
+      title : "Burger 2 lớp bò phô mai",
+      price:"65,000 VNĐ",
+      image : require('../../asset/Burger2Lop.png'),
+      quantity:"2"
+    },
+    {
+      id:3,
+      title : "3 miếng gà rán",
+      price:"101,000 VNĐ",
+      image : require('../../asset/3MiengGa.png'),
+      quantity:"1"
+    },
+    {
+        id:4,
+        title : "Burger bò phô mai đặc biệt",
+        price:"49,000 VNĐ",
+        image : require('../../asset/BurgerBoDacBiet.png'),
+        quantity:"2"
+      },
+      {
+        id:5,
+        title : "Burger 2 lớp bò phô mai",
+        price:"65,000 VNĐ",
+        image : require('../../asset/Burger2Lop.png'),
+        quantity:"2"
+      },
+      {
+        id:6,
+        title : "3 miếng gà rán",
+        price:"101,000 VNĐ",
+        image : require('../../asset/3MiengGa.png'),
+        quantity:"1"
+      },
+  ];
