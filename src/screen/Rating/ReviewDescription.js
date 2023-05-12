a23b31e89632e6eddd99a8bc6c84b3cf5bec0509
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-12 23:37:04 +0700

    Create ReviewDescription.js

---
+++ src/screen/Rating/ReviewDescription.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import Header from "../../component/Header";
+import BottomTab from "../../component/BottomTab";
+import ReviewOverall from "../../component/Review/ReviewOverall";
+import Score from "../../component/Review/Score";
+import ReviewDetail from "../../component/Review/ReviewDetail";
+
+export default function ReviewDescription (){
+    return (
+        <SafeAreaView style={{flexDirection:'column', flex:1}}>
+            <Header />
+            <ReviewOverall />
+            <Score />
+            <ReviewDetail />
+            <BottomTab />
+        </SafeAreaView>
+    )
+}
