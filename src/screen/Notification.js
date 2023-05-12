fe4487852ad8b8b570dcac2dcec8d50b7cb9b526
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-12 23:40:32 +0700

    Add files via upload

---
+++ src/screen/Notification.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ScrollView, SafeAreaView } from "react-native";
+import BottomTab from "../component/BottomTab";
+import EachNoti from "../component/NotiComponent/EachNoti";
+
+export default function Notification(){
+    return(
+        <SafeAreaView style={{flex:1}}>
+            <ScrollView>
+                <EachNoti />
+            </ScrollView>
+            <BottomTab />
+        </SafeAreaView>
+    );
+}
