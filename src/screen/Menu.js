ebcfe3ccc37a6503f22bec834af38532bcdd4a27
Phan Thanh Bách <phanbach02@gmail.com>
2023-05-17 18:01:34 +0700

    Sửa màn hình Menu
    Sửa một số chi tiết nhỏ ở màn hình Menu

--- src/screen/Menu.js
+++ src/screen/Menu.js
@@ -2,24 +2,19 @@ import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import Header from '../component/Header';
+import Header from '../component/Menu/Header';
 import Categories from '../component/Menu/Categories';
-import BottomTab from '../component/BottomTab';
 import { Divider } from "react-native-elements";
 
 const Menu = () => {
     return (
-        <SafeAreaView style={{flex: 1}}>
-            <View>
-                <StatusBar style='dark' />
-                <Header />
-            </View>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <StatusBar style='dark' />
             <ScrollView showsVerticalScrollIndicator={false}>
                 <Categories />
             </ScrollView>
             <Divider width={1} />
-            <BottomTab />
-        </SafeAreaView>
+        </View>
     );
 };
 
