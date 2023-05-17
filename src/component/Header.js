3a3dfb52bdcebafa6d5e1b915e8bfcda9ed5ba45
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-17 20:39:33 +0700

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,33 +1,93 @@
 import React from "react";
-import { View, Image, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Image, TouchableOpacity, TextInput } from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from "@react-navigation/native";
 
-export default function Header() {
-  return (
-    <View style={{
-      flexDirection:"row", 
-      justifyContent:'space-between',
-      backgroundColor: "#EA5C2B",
-      padding:15
-      }}>
-      <Image source={require('../asset/Logo.png')} />
-      <View 
-        style = {{
-          width:"60%",
-          backgroundColor:"white",
-          borderRadius:12
-        }}
-      >
-        <Image 
-          source={require('../asset/Search.png')} 
-          style={{
-            marginLeft:200,
-            marginTop:2}}
-        />
-      </View>
-      <TouchableOpacity>
-        <Image source={require('../asset/Cart.png')} />
-      </TouchableOpacity>
-    </View>    
-  );
+const Header = () => {
+    const navigation = useNavigation();
+    return (
+        <View style={styles.header}>
+            <Image
+                source={require('../asset/Logo.png')}
+                style={styles.logo}
+            />
+            <View
+                style={{
+                    flexDirection: "row",
+                    marginRight: 10,
+                    alignItems: "center",
+                    backgroundColor: 'white',
+                    borderWidth: 1,
+                    borderRadius: 20,
+                    borderColor: '#D9D9D9',
+                }}
+            >
+                <TextInput placeholder="Search..."
+                    style={{
+                        backgroundColor: 'white',
+                        color: 'white',
+                        fontSize: 16,
+                        paddingLeft: 5,
+                        borderRadius: 20,
+                        width: 187,
+
+                    }}
+                />
+                <Ionicons
+                    name="search"
+                    style={{
+                        fontSize: 25,
+                        color: "#7A7A7A",
+                        opacity: 0.8,
+                        paddingRight: 5,
+                        alignSelf: "center",
+                    }}
+                />
+            </View>
+
+
+            <TouchableOpacity onPress={() => navigation.navigate('Cart')}>
+                <Image source={require('../asset/Cart.png')} />
+            </TouchableOpacity>
+        </View>
+    );
 }
 
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: '#EA5C2B',
+        padding: 10,
+        height: 50,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    logo: {
+        height: 20,
+        width: 102,
+        marginRight: 10,
+    },
+
+    search_input: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+
+    text_input: {
+        backgroundColor: 'white',
+        width: 230,
+        height: 30,
+        right: 5,
+        borderRadius: 30,
+        padding: 6,
+
+    },
+
+    button: {
+        alignContent: 'center',
+        justifyContent: 'center'
+    }
+});
+
+export default Header;
