e2477c2e8a91910bf1cb94cef6d576b2c52e9c8c
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-05-18 15:25:35 +0700

    Update ReviewDescription.js

--- src/screen/Rating/ReviewDescription.js
+++ src/screen/Rating/ReviewDescription.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import Header from "../../component/Header";
-import BottomTab from "../../component/BottomTab";
 import ReviewOverall from "../../component/Review/ReviewOverall";
 import Score from "../../component/Review/Score";
 import ReviewDetail from "../../component/Review/ReviewDetail";
 
-export default function ReviewDescription ({navigation}){
-    return (
-        <SafeAreaView style={{flexDirection:'column', flex:1}}>
-            <Header />
-            <ReviewOverall />
-            <Score />
-            <ReviewDetail />
-            <BottomTab />
-        </SafeAreaView>
-    )
+export default function ReviewDescription() {
+  return (
+    <SafeAreaView style={{ flexDirection: "column", flex: 1 }}>
+      {/* <Header /> */}
+      <ReviewOverall />
+      <Score />
+      <ReviewDetail />
+    </SafeAreaView>
+  );
 }
