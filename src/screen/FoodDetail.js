c66910d4083d0c71941a192ab697479bdca0f45e
Phan Thanh Bách <phanbach02@gmail.com>
2023-05-19 21:16:48 +0700

    Sửa FoodDetail

--- src/screen/FoodDetail.js
+++ src/screen/FoodDetail.js
@@ -1,22 +1,35 @@
-import { Image, Button, StyleSheet, Text, View, TouchableOpacity, TouchableHighlight } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View, TouchableOpacity, Pressable, ScrollView, Dimensions } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { FontAwesome } from '@expo/vector-icons';
-import { ScrollView } from 'react-native-gesture-handler';
 import { FontAwesome5 } from '@expo/vector-icons';
 
+const tabs = ['Chi tiết', 'Đánh giá'];
 
 const FoodDetail = ({ navigation, route }) => {
+  const [selected, setSelected] = useState(0);
+
+  const onScroll = ({ nativeEvent }) => {
+    const index = Math.round(nativeEvent.contentOffset.x / (deviceWidth - 20));
+
+    setSelected(index);
+  };
+
   const item = route.params;
 
   return (
-    <View style={{ backgroundColor: 'white', }}> 
-      <View style={styles.header}>
-        <Icon style={{ color: '#FF7F3F' }} name="chevron-left" size={40} onPress={navigation.goBack}></Icon>
-        <Text style={{ fontWeight: 700, fontSize: 20, color: '#FF7F3F' }}>Chi Tiết Sản Phẩm</Text>
-      </View> 
+    <View style={{ backgroundColor: 'white', flex: 1 }}>
+
+      {/* <View style={styles.header}>
+        <TouchableOpacity onPress={navigation.goBack}>
+          <Icon style={{ color: 'black', textAlign: 'center' }} name="chevron-left" size={30}></Icon>
+        </TouchableOpacity>
+        <View style={styles.title}>
+          <Text style={{ fontWeight: 700, fontSize: 18, color: 'black', }}>Chi tiết sản phẩm</Text>
+        </View>
+      </View> */}
       <ScrollView showsVerticalScrollIndicator={true}>
         <View style={styles.backgroundDetails}>
           <Image source={item.image} style={{ height: 220, width: 220 }} />
@@ -24,14 +37,15 @@ const FoodDetail = ({ navigation, route }) => {
 
         <View style={{ marginHorizontal: 15, }}>
 
-          <Text style={{ fontWeight: 700, fontSize: 24, color: '#FF7F3F', marginBottom: 8, }}>{item.name}</Text>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Text style={{ fontWeight: 700, fontSize: 28, marginBottom: 8, }}>{item.price}</Text>
-            <TouchableOpacity>
-            <View style={{ marginRight: 25, right: -210,  }}>
-              <MaterialCommunityIcons style={{backgroundColor: '#FF7F3F', borderRadius: 200, paddingHorizontal: 16, paddingVertical: 14,}} name="cart-plus" size={26} color="white" />
+          <Text style={{ fontWeight: 700, fontSize: 20, color: '#EA5C2B', marginBottom: 8, }}>{item.name}</Text>
+          <View style={{ flexDirection: 'row', width: deviceWidth }}>
+            <Text style={{ fontWeight: 700, fontSize: 25, marginBottom: 8, }}>{item.price} VNĐ</Text>
+
+            <View style={{ marginRight: 25, right: -170, }}>
+              <TouchableOpacity>
+                <MaterialCommunityIcons style={{ backgroundColor: '#EA5C2B', borderRadius: 200, paddingHorizontal: 16, paddingVertical: 14, }} name="cart-plus" size={26} color="white" />
+              </TouchableOpacity>
             </View>
-            </TouchableOpacity>
           </View>
 
           <View style={{ flexDirection: 'row', marginBottom: 15 }}>
@@ -45,40 +59,64 @@ const FoodDetail = ({ navigation, route }) => {
             <Text style={{ marginLeft: 10, fontWeight: 700, fontSize: 16, }}>(17 đánh giá)</Text>
           </View>
 
-          <View style={{ flexDirection: 'row', marginVertical: 15, }}>
-            <Text style={[styles.textCate, styles.textStand]}>Chi tiết</Text>
+          {/* <View style={{ flexDirection: 'row', marginVertical: 15, }}>
+            <Text style={[styles.textCate, { color: '#837878' }]} onPress={() => navigation.navigate('FoodDetail', item)} >Chi tiết</Text>
+            <Text style={[styles.textCate, styles.textStand]}>Đánh giá</Text>
           </View>
 
           <View style={{ height: 260, width: '100%', }}>
-            <Text style={{ fontSize: 16, color: '#837878', fontWeight: 700, lineHeight: 30, }}>{item.details}</Text>
             <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end' }}>
               <TouchableOpacity style={styles.contactButton}>
-                <Text style={{ fontSize: 16, fontWeight: 700, color: 'white', marginHorizontal: 5, }} onPress={() => navigation.navigate('ReviewDescription')}>Xem đánh giá</Text>
+                <Text style={{ fontSize: 14, fontWeight: 700, color: 'white', marginHorizontal: 5, }}>Xem thêm</Text>
                 <FontAwesome5 name="angle-double-right" size={14} color="white" />
               </TouchableOpacity>
             </View>
 
-          </View>
+          </View> */}
+        </View>
+        <View style={styles.container}>
+        <Text style={[styles.textCate, styles.textStand]}>Chi tiết</Text>
+          <ScrollView
+            horizontal
+            pagingEnabled
+            snapToAlignment="center"
+            onScroll={onScroll}
+            decelerationRate="fast">
+            <View style={{ width: deviceWidth - 30, }}>
+              <Text style={{ fontSize: 16, color: '#837878', fontWeight: 700, lineHeight: 30, }}>{item.details}</Text>
+              <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginBottom: 10 }}>
+                <TouchableOpacity style={styles.contactButton}>
+                  <Text style={{ fontSize: 14, fontWeight: 700, color: 'white', marginHorizontal: 5, }} onPress={() => navigation.navigate('ReviewStack', route)}>Xem đánh giá</Text>
+                  <FontAwesome5 name="angle-double-right" size={14} color="white" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ScrollView>
         </View>
 
       </ScrollView>
-    </View >
+    </View>
   )
 }
 
 export default FoodDetail
 
+const deviceWidth = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
   header: {
-    paddingVertical: 10,
+    width: deviceWidth,
     flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 16,
+    backgroundColor: '#FF7F3F',
+    padding: 10,
     backgroundColor: 'white',
-    borderBottomWidth: 0.5, 
-    borderBottomColor: 'rgba(122, 122, 122, 0.5)',
+    elevation: 2,
   },
-
+  title: {
+    width: deviceWidth,
+    justifyContent: 'center',
+    // alignItems: 'center',
+  },
   backgroundDetails: {
     justifyContent: 'center',
     alignItems: 'center', height: 240,
@@ -88,13 +126,22 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 12,
     borderTopRightRadius: 12,
     marginBottom: 20,
-  }
-  ,
+  },
+  container: {
+    marginHorizontal: 15,
+  },
+
   details: {
     paddingHorizontal: 20,
     paddingTop: 40,
     paddingBottom: 60,
     backgroundColor: '#FF7F3F'
+  },
+  line: {
+    width: '80%',
+    height: 2,
+    backgroundColor: '#FF7F3F',
+    alignItems: 'center',
   },
   starShine: {
     flexDirection: 'row',
@@ -102,17 +149,27 @@ const styles = StyleSheet.create({
   fiveStar: {
     paddingRight: 5,
   },
+  tabs: {
+    marginVertical: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    backgroundColor: 'white',
+  },
   textCate: {
     fontWeight: 700,
     fontSize: 18,
     paddingBottom: 8,
     marginRight: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   textStand: {
     color: '#FF7F3F',
     paddingRight: 5,
     borderBottomColor: '#FF7F3F',
-    borderBottomWidth: 1
+    borderBottomWidth: 2,
+    width: 80,
   },
 
   contactButton: {
@@ -121,8 +178,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    width: 150,
-    height: 30,
+    width: 115,
+    height: 25,
     marginTop: 10,
   },
 
