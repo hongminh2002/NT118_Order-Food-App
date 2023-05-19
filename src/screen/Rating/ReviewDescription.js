a0428823c31c96eaf7b5d07647fb60292ef85a14
Phan Thanh Bách <phanbach02@gmail.com>
2023-05-19 21:39:31 +0700

    Sửa màn hình ReviewDes

--- src/screen/Rating/ReviewDescription.js
+++ src/screen/Rating/ReviewDescription.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
-import Header from "../../component/Header";
+import { View } from "react-native";
 import ReviewOverall from "../../component/Review/ReviewOverall";
 import Score from "../../component/Review/Score";
 import ReviewDetail from "../../component/Review/ReviewDetail";
 
-export default function ReviewDescription() {
-  return (
-    <SafeAreaView style={{ flexDirection: "column", flex: 1 }}>
-      {/* <Header /> */}
-      <ReviewOverall />
-      <Score />
-      <ReviewDetail />
-    </SafeAreaView>
-  );
+const ReviewDescription = () => {
+    return (
+        <View style={{flexDirection:'column', flex:1}}>
+            <Score />
+            <ReviewDetail />
+        </View>
+    )
 }
+
+export default ReviewDescription;
