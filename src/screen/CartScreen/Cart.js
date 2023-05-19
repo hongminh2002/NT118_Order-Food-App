d55a8da0cb0a40df38f5ec84aa686e78d7388381
Phan Thanh Bách <phanbach02@gmail.com>
2023-05-19 21:32:30 +0700

    Sửa màn hình Cart và OrderConfirm

--- src/screen/CartScreen/Cart.js
+++ src/screen/CartScreen/Cart.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
-import Header from "../../component/Header";
-import BottomTab from "../../component/BottomTab";
 import CartItem from "../../component/CartDetail/CartItem";
 import CheckOut from "../../component/CartDetail/CheckOut";
 
 export default function Cart() {
   return (
-    <SafeAreaView style={{flex:1}}>
-        <Header />
+    <View style={{flex:1}}>
         <CartItem />
         <CheckOut />
-        <BottomTab />
-    </SafeAreaView>
+    </View>
   );
 }
--- src/screen/OrderConfirm.js
+++ src/screen/OrderConfirm.js
@@ -2,7 +2,6 @@ import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Title from '../component/OrderConfirm/Title';
 import UserInformation from '../component/OrderConfirm/UserInformation';
 import OrderDetail from '../component/OrderConfirm/OrderDetail';
 import Footer from '../component/OrderConfirm/Footer';
@@ -10,13 +9,10 @@ import { Divider } from 'react-native-elements';
 
 const OrderConfirm = () => {
     return (
-        <SafeAreaView style={{ backgroundColor: '#D9D9D9', flex: 1 }}>
+        <View style={{ backgroundColor: '#D9D9D9', flex: 1 }}>
             <StatusBar style='auto' />
-            <View style={{ marginBottom: 15 }}>
-                <Title />
-            </View>
             <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={{ marginBottom: 15 }}>
+            <View style={{ marginVertical: 15 }}>
                 <UserInformation />
             </View>
             <View style={{ marginBottom: 15 }}>
@@ -25,7 +21,7 @@ const OrderConfirm = () => {
             </ScrollView>
             <Divider width={1} />
             <Footer />
-        </SafeAreaView>
+        </View>
     );
 };
 
