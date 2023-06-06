f743fd3e42f3b2dc2d4ca37d1af4b63c701da851
Vân Anh <92537419+vananh3040@users.noreply.github.com>
2023-06-06 23:25:59 +0700

    Thêm chức năng quên mật khẩu

--- src/screen/QuenMatKhau.js
+++ src/screen/QuenMatKhau.js
@@ -1,31 +1,61 @@
-import React, {Component} from 'react';
-import {Text, TextInput, SafeAreaView, StyleSheet, TouchableOpacity, View, Image} from 'react-native';
+import React, { useState, useEffect, Component } from 'react';
+
+import { Text, TextInput, SafeAreaView, StyleSheet, TouchableOpacity, View, Image } from 'react-native';
 import Constants from "expo-constants";
 import { SimpleLineIcons } from '@expo/vector-icons';
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
-const QuenMatKhau = ({navigation}) => {
+
+const QuenMatKhau = ({ navigation }) => {
+
+  const [email, setEmail] = useState(null);
+
+  //reset password
+
+  const resetPassword = () => {
+    if (email != null) {
+      const auth = getAuth();
+
+       sendPasswordResetEmail(auth, email)
+        .then(() => {
+           // ..
+          alert("Password reset email sent successfully!")
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          alert(errorMessage);
+        });
+
+    }
+    else {
+      alert("Please enter valid email");
+    }
+  }
+
   return (
     <SafeAreaView style={style.container}>
-      <Image source={require('../asset/Nền1.png')} style={style.background1}/>
-      <Image source={require('../asset/Nền2.png')} style={style.background2}/>
+      <Image source={require('../asset/Nền1.png')} style={style.background1} />
+      <Image source={require('../asset/Nền2.png')} style={style.background2} />
       <View style={style.page}>
         <View>
           <Text style={style.pageName}>
             Quên mật khẩu
           </Text>
         </View>
-        <View style={{flex:1, justifyContent: 'space-between'}}>
+        <View style={{ flex: 1, justifyContent: 'space-between' }}>
           <View style={style.content}>
-            <Text style={style.nhap}>Nhập số điện thoại</Text>
+            <Text style={style.nhap}>Nhập email của bạn</Text>
             <View style={style.input}>
               <View style={style.icon}>
-                <SimpleLineIcons name="phone" size={14} color="#B9B9B9"/>
+                <SimpleLineIcons name="user" size={14} color="#B9B9B9" />
               </View>
               <View style={style.type}>
-                <TextInput style={style.type} placeholder="Số điện thoại"/>
+                <TextInput value={email}
+                  onChangeText={(text) => setEmail(text)} style={style.type} placeholder="Email" />
               </View>
             </View>
-            <TouchableOpacity onPress={() => navigation.navigate('XacThuc')}>
+            <TouchableOpacity onPress={() => resetPassword()/**navigation.navigate('XacThuc')**/}>
               <Text style={style.send}>Gửi</Text>
             </TouchableOpacity>
           </View>
@@ -46,34 +76,34 @@ const TEXT = {
 };
 
 const style = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     textAlign: "center",
     backgroundColor: "#fff",
     paddingTop: Constants.statusBarHeight,
   },
   background1: {
-    position:'absolute',
+    position: 'absolute',
     width: '100%',
     height: 450,
   },
   background2: {
-    position:'absolute',
+    position: 'absolute',
     top: 100,
     width: '80%',
     height: 700,
     borderRadius: 30,
-    alignSelf:'center',
+    alignSelf: 'center',
   },
   page: {
     top: 100,
     width: '80%',
     height: 450,
-    alignSelf:'center',
+    alignSelf: 'center',
   },
-  pageName : {
+  pageName: {
     ...TEXT,
-    marginTop:40,
+    marginTop: 40,
     color: "#EA5C2B",
     fontSize: 18,
     fontWeight: "bold",
@@ -89,7 +119,7 @@ const style = StyleSheet.create({
     color: "#EA5C2B",
   },
   input: {
-    flexDirection:'row',
+    flexDirection: 'row',
     marginTop: 9,
     height: 39,
     borderRadius: 12,
@@ -97,13 +127,13 @@ const style = StyleSheet.create({
     borderColor: '#B9B9B9',
   },
   icon: {
-    flex:1,
+    flex: 1,
     top: 10,
     left: 15,
   },
   type: {
     fontSize: 12,
-    flex:4,
+    flex: 4,
     color: '#B9B9B9',
   },
   send: {
