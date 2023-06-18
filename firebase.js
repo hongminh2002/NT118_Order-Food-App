f2d4ea4691897fc1387379fec5524828a2fefd42
Phan Thanh Bách <phanbach02@gmail.com>
2023-06-18 17:32:42 +0700

    Cập nhật phương thức firebase
    Thêm một số hàm

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, collection, addDoc, getDocs, setDoc, doc } from "firebase/firestore";
+import { getFirestore, collection, collectionGroup, addDoc, getDoc, getDocs, setDoc, updateDoc, doc, onSnapshot, query, where} from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -29,7 +29,7 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 
-export { auth, app, db, storage, getFirestore, collection, addDoc, getDocs, setDoc, doc };
+export { auth, app, db, storage, collection, collectionGroup, addDoc, getDoc, getDocs, setDoc, updateDoc, doc, onSnapshot, query, where };
 
 // // Import the functions you need from the SDKs you need
 // //import { initializeApp } from "firebase/app";
