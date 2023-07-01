79f44e451138b669c15fbab4bf0d62d555c64dd2
Phan Thanh Bách <phanbach02@gmail.com>
2023-07-01 17:18:20 +0700

    Thay đổi màn hình OrderTracking

--- src/screen/OrderTracking.js
+++ src/screen/OrderTracking.js
@@ -1,24 +1,56 @@
-import { View, Text, ScrollView, FlatList, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { View, Text, Dimensions } from 'react-native'
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
-import Title from '../component/OrderTracking/Title';
-import TopTab from '../component/OrderTracking/TopTab';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Ongoing from '../component/OrderTracking/Ongoing';
+import Delivering from '../component/OrderTracking/Delivering';
+import CompletedOrder from '../component/OrderTracking/CompletedOrder';
+import Cancelled from '../component/OrderTracking/Cancelled';
+import Waiting from '../component/OrderTracking/Waiting';
+import { useFonts } from "expo-font";
+
+const Tab = createMaterialTopTabNavigator();
 
 const OrderTracking = () => {
+    const [fontsLoaded] = useFonts({
+        "Roboto-Bold": require("../asset/fonts/Roboto-Bold.ttf"),
+        "Roboto-Medium": require("../asset/fonts/Roboto-Medium.ttf"),
+        "Roboto-Regular": require("../asset/fonts/Roboto-Regular.ttf"),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
 
     return (
-        <SafeAreaProvider>
-            <NavigationContainer independent={true}>
-                <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF' }}>
-                    <StatusBar style="auto" />
-                    <Title />
-                    <TopTab />
-                </SafeAreaView>
-            </NavigationContainer>
-        </SafeAreaProvider>
-    );
-};
+        <Tab.Navigator screenOptions={{
+            tabBarLabelStyle: { fontSize: 12, fontFamily: 'Roboto-Medium', textTransform: 'none', },
+            tabBarItemStyle: {
+                width: deviceWidth / 5,
+                flex: 1,
+                borderRadius: 10,
+            },
+            tabBarStyle: { justifyContent: 'center' },
+            tabBarActiveTintColor: '#EA5C2B',
+            tabBarInactiveTintColor: '#7A7A7A',
+            tabBarAllowFontScaling: true,
+            tabBarIndicatorStyle: {
+                backgroundColor: '#rgba(255, 127, 63, 0.25)',
+                height: 45,
+                marginBottom: 5,
+                borderRadius: 12
+            }
+        }}>
+            <Tab.Screen name="Chờ xác nhận" component={Waiting} />
+            <Tab.Screen name="Đang thực hiện" component={Ongoing} />
+            <Tab.Screen name="Đang vận chuyển" component={Delivering} />
+            <Tab.Screen name="Đã hoàn thành" component={CompletedOrder} />
+            <Tab.Screen name="Đã hủy" component={Cancelled} />
+        </Tab.Navigator>
+    )
+}
+
+const deviceWidth = Math.round(Dimensions.get("window").width);
 
 export default OrderTracking;
