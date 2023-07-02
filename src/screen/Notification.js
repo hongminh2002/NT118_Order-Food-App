c156a7af81ebd41fac2ae0ffbafc7965f85001d4
Hồng Minh Trần <80388297+hongminh2002@users.noreply.github.com>
2023-07-02 08:14:51 +0700

    Update Notification.js

--- src/screen/Notification.js
+++ src/screen/Notification.js
@@ -9,7 +9,6 @@ export default function Notification(){
             <ScrollView>
                 <EachNoti />
             </ScrollView>
-            <BottomTab />
         </SafeAreaView>
     );
 }
